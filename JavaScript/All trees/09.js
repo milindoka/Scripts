c82ff9f148847f80fe7815030d2ae457977bd901
milindoka <oak445@gmail.com>
2020-08-21 08:34:21 +0530

    Shifted fill key array procedure to separate JS file

--- JavaScript/All trees/09.js
+++ JavaScript/All trees/09.js
@@ -28,6 +28,37 @@ if (testRE && testRE.length > 1)  return testRE[1];     //RegEx has found key so
 }
 
 
+function FillkeyArray(keyarray,data)
+{
+    var keycount=2;
+
+    //split mytext by nextline = "\n" 
+    var myArray = data.split("\n");
+     
+    for(i=0;i<myArray.length;i++)
+    { var res = myArray[i].replace("<", " ");
+      var res1 =res.replace(">", " ");   
+      var res2 =res1.replace("\\", " ");   ////remove problemetic chars
+      
+      var str=key(res2);
+      if(str==="") continue;
+      var found=false;
+      var arrlen = keyarray.length;
+      for (var j =0; j < arrlen; j++)
+               {   //var result = string1.localeCompare(string2);
+                   if(str===keyarray[j])  // key already exists
+                    {  found=true; break; }
+          
+                }
+
+      if(found) continue;
+
+      keyarray[keycount]=str;
+      keycount++;
+ //     alert(str);  
+     }
+}
+
 
 
 
